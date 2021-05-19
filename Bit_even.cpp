e5735cc894adb6736b35f398fd51fe2c3d5c5f17
yaminimahitha <84062507+yaminimahitha@users.noreply.github.com>
2021-05-19 21:08:25 +0530

    Add files via upload

---
+++ Bit_even.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num;
+	int no=1;
+	cout<<"Enter the number"<<endl;
+	cin>>num;
+	if(num&no==1)
+	{
+		cout<<"odd number";
+	}
+	else
+	cout<<"Even number";
+	return 0;
+	
+}
